7917a963810af8dec8e6966b31e64a89f61f0d3e
Gus Luxton <gus@gravitational.com>
2020-05-15 16:39:15 -0300

    Revert "Try a change to GetFreeTCPPorts"
    This reverts commit 5728cabc7d7f6259edce0308de5fc8e329545a14.

--- vendor/github.com/gravitational/teleport/lib/utils/utils.go
+++ vendor/github.com/gravitational/teleport/lib/utils/utils.go
@@ -361,23 +361,18 @@ func (p *PortList) PopIntSlice(num int) []int {
 	return ports
 }
 
-// GetFreeTCPPorts returns n free ports (which are suggested by the kernel)
-func GetFreeTCPPorts(n int) (PortList, error) {
+// PortStartingNumber is a starting port number for tests
+const PortStartingNumber = 20000
+
+// GetFreeTCPPorts returns n ports starting from port 20000.
+func GetFreeTCPPorts(n int, offset ...int) (PortList, error) {
 	list := make(PortList, 0, n)
-	for i := 0; i < n; i++ {
-		addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		listen, err := net.ListenTCP("tcp", addr)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		defer listen.Close()
-
-		port := strconv.Itoa(listen.Addr().(*net.TCPAddr).Port)
-		list = append(list, port)
+	start := PortStartingNumber
+	if len(offset) != 0 {
+		start = offset[0]
+	}
+	for i := start; i < start+n; i++ {
+		list = append(list, strconv.Itoa(i))
 	}
 	return list, nil
 }
