0d5c8c1fcda81622fad6d5e4832c7ff13ec19a28
Nate Gadzhibalaev <nate@respawn.io>
2020-12-05 01:57:43 +0000

    Improved user traits HCL syntax

--- terraform/provider/provider.go
+++ terraform/provider/provider.go
@@ -41,6 +41,7 @@ func Provider() terraform.ResourceProvider {
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"teleport_user": resourceTeleportUser(),
+			"teleport_role": resourceTeleportRole(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
