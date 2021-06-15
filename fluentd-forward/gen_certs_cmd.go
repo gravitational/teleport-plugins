228826f59b8be97dc672c596988acb99d2610863
Victor Sokolov <gzigzigzeo@gmail.com>
2021-06-15 23:17:46 +0300

    GenCerts (WIP)

--- fluentd-forward/gen_certs_cmd.go
+++ fluentd-forward/gen_certs_cmd.go
@@ -16,5 +16,74 @@ limitations under the License.
 
 package main
 
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"time"
+
+	"github.com/gravitational/trace"
+)
+
 type GenCertsCmd struct {
+	// Out path and file prefix to put certificates into
+	Out string `arg:"true" help:"Output directory and file prefix" required:"true"`
+
+	// Pwd key passphrase
+	Pwd string `arg:"true" help:"Passphrase" required:"true"`
+
+	// Certificate TTL
+	TTL time.Duration `arg:"true" help:"Certificate TTL" required:"true" default:"10800m"`
+
+	// Length is RSA key length
+	Length int `help:"Key length" enum:"1024,2048,4096" default:"2048"`
+
+	// CN certificate common name
+	CN string `help:"Certificate common name" default:"localhost"`
+}
+
+// Run runs the generator
+func (c *GenCertsCmd) Run() error {
+	entity := pkix.Name{
+		CommonName:   c.CN,
+		Country:      []string{"US"},
+		Organization: []string{c.CN},
+	}
+
+	ca := &x509.Certificate{
+		SerialNumber:          big.NewInt(2019),
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(c.TTL),
+		Subject:               entity,
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	caPrivKey, err := rsa.GenerateKey(rand.Reader, c.Length)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return err
+	}
+
+	caPEM := new(bytes.Buffer)
+	pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+
+	caPrivKeyPEM := new(bytes.Buffer)
+	pem.Encode(caPrivKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
+	})
 }
