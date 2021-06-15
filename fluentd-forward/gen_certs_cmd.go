dda4c11ac8e63eb99a606941d91081ffe2271ba1
Victor Sokolov <gzigzigzeo@gmail.com>
2021-06-16 01:52:14 +0300

    Updates to gen certs

--- fluentd-forward/gen_certs_cmd.go
+++ fluentd-forward/gen_certs_cmd.go
@@ -25,6 +25,7 @@ import (
 	"io/ioutil"
 	"math/big"
 	"net"
+	"path"
 	"time"
 
 	"github.com/gravitational/trace"
@@ -32,7 +33,7 @@ import (
 
 type GenCertsCmd struct {
 	// Out path and file prefix to put certificates into
-	Out string `arg:"true" help:"Output directory and file prefix" required:"true" type:"existingdir"`
+	Out string `arg:"true" help:"Output directory" type:"existingdir" required:"true"`
 
 	// Pwd key passphrase
 	Pwd string `arg:"true" help:"Passphrase" required:"true"`
@@ -40,152 +41,161 @@ type GenCertsCmd struct {
 	// Certificate TTL
 	TTL time.Duration `help:"Certificate TTL" required:"true" default:"87600h"`
 
-	// Hosts is a subjectAltNames for server cert
-	Hosts []string `help:"Certificate SAN hosts" default:"localhost"`
+	// DNSNames is a DNS subjectAltNames for server cert
+	DNSNames []string `help:"Certificate SAN hosts" default:"localhost"`
 
-	// HostNames is a subjectAltNames for server cert
+	// HostNames is an IP subjectAltNames for server cert
 	IP []string `help:"Certificate SAN IPs"`
 
 	// Length is RSA key length
 	Length int `help:"Key length" enum:"1024,2048,4096" default:"2048"`
 
 	// CN certificate common name
-	CN string `help:"Certificate common name" default:"localhost"`
+	CN string `help:"Common name for server cert" default:"localhost"`
 }
+
+var (
+	// maxBigInt is a reader for serial number random
+	maxBigInt *big.Int = new(big.Int).Lsh(big.NewInt(1), 128)
+)
 
 // Run runs the generator
 func (c *GenCertsCmd) Run() error {
-	// entity := pkix.Name{
-	// 	CommonName:   c.CN,
-	// 	Country:      []string{"US"},
-	// 	Organization: []string{c.CN},
-	// }
+	entity := pkix.Name{
+		CommonName: c.CN,
+		Country:    []string{"US"},
+	}
 
-	ca := &x509.Certificate{
-		SerialNumber:          big.NewInt(2019),
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(c.TTL),
-		Subject:               pkix.Name{CommonName: "ca"},
+	// CA CSR
+	sn, err := rand.Int(rand.Reader, maxBigInt)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	notBefore := time.Now()
+	notAfter := time.Now().Add(c.TTL)
+
+	caCert := &x509.Certificate{
+		SerialNumber:          sn,
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
 		IsCA:                  true,
 		MaxPathLenZero:        true,
 		KeyUsage:              x509.KeyUsageCRLSign | x509.KeyUsageCertSign,
 		BasicConstraintsValid: true,
 	}
 
-	// Server key
-	// ca.DNSNames = c.Hosts
-	// if len(c.IP) == 0 {
-	// 	ips, _ := net.LookupIP("localhost")
-	// 	if ips != nil {
-	// 		ca.IPAddresses = append(ips, net.ParseIP("::1"))
-	// 	}
-	// } else {
-	// 	for ip, _ := range c.IP {
-	// 		ca.IPAddresses = append(ca.IPAddresses, net.ParseIP(ip))
-	// 	}
-	// }
-
-	caPrivKey, err := rsa.GenerateKey(rand.Reader, c.Length)
+	// Client CSR
+	sn, err = rand.Int(rand.Reader, maxBigInt)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return err
+	clientCert := &x509.Certificate{
+		SerialNumber: sn,
+		Subject:      entity,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
 	}
 
-	caBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: caBytes})
-	caPkBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey)})
-
-	err = ioutil.WriteFile("example/keys/ca.crt", caBytesPEM, 0444)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	err = ioutil.WriteFile("example/keys/ca.key", caPkBytesPEM, 0444)
+	sn, err = rand.Int(rand.Reader, maxBigInt)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	// SERVER
-
-	cert := &x509.Certificate{
-		SerialNumber: big.NewInt(1658),
-		Subject:      pkix.Name{CommonName: "server"},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		//SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		KeyUsage:    x509.KeyUsageDigitalSignature,
+	// Server CSR
+	serverCert := &x509.Certificate{
+		SerialNumber: sn,
+		Subject:      entity,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 	}
 
-	cert.DNSNames = c.Hosts
+	// Append subjectAltNames
+	serverCert.DNSNames = c.DNSNames
 	if len(c.IP) == 0 {
-		ips, _ := net.LookupIP("localhost")
-		if ips != nil {
-			cert.IPAddresses = append(ips, net.ParseIP("::1"))
+		for _, name := range c.DNSNames {
+			ips, err := net.LookupIP(name)
+			if err != nil {
+				return trace.Wrap(err)
+			}
+
+			if ips != nil {
+				serverCert.IPAddresses = append(serverCert.IPAddresses, ips...)
+			}
 		}
 	} else {
 		for _, ip := range c.IP {
-			cert.IPAddresses = append(cert.IPAddresses, net.ParseIP(ip))
+			serverCert.IPAddresses = append(serverCert.IPAddresses, net.ParseIP(ip))
 		}
 	}
 
-	certPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return err
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return err
-	}
-
-	certBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
-	certPkBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(certPrivKey)})
-
-	err = ioutil.WriteFile("example/keys/server.crt", certBytesPEM, 0444)
+	// Generate CA key and certificate
+	caPK, err := rsa.GenerateKey(rand.Reader, c.Length)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	err = ioutil.WriteFile("example/keys/server.key", certPkBytesPEM, 0444)
+	caCertBytes, err := x509.CreateCertificate(rand.Reader, caCert, caCert, &caPK.PublicKey, caPK)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	// client
-
-	client := &x509.Certificate{
-		SerialNumber: big.NewInt(16538),
-		Subject:      pkix.Name{CommonName: "client"},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		//SubjectKeyId: []byte{1, 2, 3, 4, 6, 9},
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-		KeyUsage:    x509.KeyUsageDigitalSignature,
-	}
-
-	clientPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return err
-	}
-
-	clientBytes, err := x509.CreateCertificate(rand.Reader, client, ca, &clientPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return err
-	}
-
-	clientBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: clientBytes})
-	clientPkBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(clientPrivKey)})
-
-	err = ioutil.WriteFile("example/keys/client.crt", clientBytesPEM, 0444)
+	err = c.writeKeyAndCert(path.Join(c.Out, "ca"), caCertBytes, caPK)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	err = ioutil.WriteFile("example/keys/client.key", clientPkBytesPEM, 0444)
+	// Generate server key and certificate
+	serverPK, err := rsa.GenerateKey(rand.Reader, c.Length)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	serverCertBytes, err := x509.CreateCertificate(rand.Reader, serverCert, caCert, &serverPK.PublicKey, caPK)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	err = c.writeKeyAndCert(path.Join(c.Out, "server"), serverCertBytes, serverPK)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	// Generate client key and certificate
+	clientPK, err := rsa.GenerateKey(rand.Reader, c.Length)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	clientCertBytes, err := x509.CreateCertificate(rand.Reader, clientCert, caCert, &clientPK.PublicKey, caPK)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	err = c.writeKeyAndCert(path.Join(c.Out, "client"), clientCertBytes, clientPK)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
+}
+
+// writeKeyAndCert writes private key and certificate on disk
+func (c *GenCertsCmd) writeKeyAndCert(prefix string, certBytes []byte, pk *rsa.PrivateKey) error {
+	caBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
+	caPkBytesPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
+
+	err := ioutil.WriteFile(prefix+".crt", caBytesPEM, 0444)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	err = ioutil.WriteFile(prefix+".key", caPkBytesPEM, 0444)
 	if err != nil {
 		return trace.Wrap(err)
 	}
