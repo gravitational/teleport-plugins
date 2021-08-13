cab9f9ea1319fc3be10fda090a1837df37cb11a4
Russell Jones <rjones@gravitational.com>
2021-08-13 20:03:46 +0000

    Fix.

--- scripts/go.mod
+++ scripts/go.mod
@@ -4,7 +4,12 @@ go 1.16
 
 require (
 	github.com/drone/drone-go v1.6.0
-	github.com/google/go-github/v38 v38.1.0 // indirect
+	github.com/google/go-github/v38 v38.1.0
+	github.com/gravitational/trace v1.1.15
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210326060303-6b1517762897 // indirect
 	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5
+	gopkg.in/yaml.v2 v2.2.2
 )
--- scripts/release.go
+++ scripts/release.go
@@ -1,97 +1,195 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
+	"text/template"
 	"time"
 
+	"golang.org/x/mod/semver"
 	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/google/go-github/v38/github"
+	"github.com/gravitational/trace"
 )
 
-var releaseList = []string{
-	"teleport-event-handler",
-	"teleport-jira",
-	"teleport-mattermost",
-	"teleport-slack",
-	"teleport-pagerduty",
-	"terraform-provider-teleport",
-}
+func readFlags() (string, string, string, error) {
+	tag := flag.String("tag", "", "tag of Teleport and Teleport Plugins to release")
+	teleportCommit := flag.String("teleport-commit", "", "Teleport commit to tag and release")
+	pluginsCommit := flag.String("plugins-commit", "", "Teleport Plugins commit to tag and release")
 
-func readFlags() (string, error) {
-	version := flag.String("version", "", "version of plugins to release")
 	flag.Parse()
 
-	if version == "" {
-		log.Fatalf("Version string is missing.")
+	if *tag == "" || *teleportCommit == "" || *pluginsCommit == "" {
+		return "", "", "", fmt.Errorf("usage: release -tag v1.2.3 --teleport-commit 419119f8 --plugins-commit a59afea8")
+	}
+	if semver.Canonical(*tag) == "" {
+		return "", "", "", fmt.Errorf("invalid tag %v", *tag)
 	}
 
+	return *tag, *teleportCommit, *pluginsCommit, nil
 }
 
-func readCredentials() (string, string, error) {
+func buildClients() (drone.Client, *github.Client, error) {
+	// Read in Drone credentials.
 	droneServer := os.Getenv("DRONE_SERVER")
 	droneToken := os.Getenv("DRONE_TOKEN")
 
-	if droneServer == "" || droneToken == "" {
-		return "", "", fmt.Errorf("credentials missing")
+	// Read in GitHub credentials.
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, nil, err
 	}
-	return droneServer, droneToken, nil
-
-	client := github.NewClient(nil)
-
-	//droneServer, droneToken, err := readCredentials()
-	//if err != nil {
-	//	log.Fatalf("Failed to read in credentials: %v.", err)
-	//}
-
-}
-
-func prepareReleases(version string) ([]string, error) {
-}
-
-func createTags(client github.Client, version string) error {
-	tags, _, _ := client.Repositories.ListTags(context.Background(), "gravitational", "teleport-plugins", nil)
-
-	for _, tag := range tags {
-		fmt.Printf("--> tag: %v.\n", *tag.Name)
+	bytes, err := ioutil.ReadFile(filepath.Join(homedir, ".config", "gh", "hosts.yml"))
+	if err != nil {
+		return nil, nil, err
 	}
-}
+	var config ghConfig
+	if err := yaml.Unmarshal(bytes, &config); err != nil {
+		return nil, nil, trace.Wrap(err)
+	}
+	ghToken := config.Host.Token
 
-func droneBuild(droneServer string, droneToken string, version string) error {
+	// Validate all credentials are available.
+	if droneServer == "" || droneToken == "" || ghToken == "" {
+		fmt.Printf(`Failed to find needed credentials: to use this release script you need
+credentials for Drone (to monitor and promote builds) and GitHub (to create
+tags and releases).
 
-	// Prepare the release tags that will be created for this release.
-	releases := prepareReleases(version)
+Found the following credentials:
 
-	// Create Drone client authenticted with ambient credentials.
-	config := new(oauth2.Config)
-	auther := config.Client(
+  droneServer: %v
+  droneToken:  %v
+  ghToken:     %v
+
+If you are missing Drone credentials, navigate to https://drone.teleport.dev
+click on the user icon in the top left corner of the screen and then click on
+"User settings". Once on the "User settings" page, copy the environment
+variables listed under "Example CLI Usage" to your terminal.
+
+If you are missing GitHub credentials, navigate to https://cli.github.com and
+install the "gh" CLI tool and type "gh auth login" to fetch GitHub credentials
+for your account.
+
+`, droneServer, droneToken, ghToken)
+		return nil, nil, fmt.Errorf("credentials missing")
+	}
+
+	// Build Drone and GitHub clients.
+	droneConfig := new(oauth2.Config)
+	droneClient := drone.NewClient(droneServer, droneConfig.Client(
 		oauth2.NoContext,
 		&oauth2.Token{
 			AccessToken: droneToken,
 		},
-	)
-	client := drone.NewClient(droneServer, auther)
+	))
+	ghClient := github.NewClient(oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: ghToken,
+		},
+	)))
 
-	// Wait for Drone to finish building artifacts for the tag.
-	if err := waitForTag(client, version, ""); err != nil {
-		return err
-	}
+	return droneClient, ghClient, nil
+}
 
-	//// Promote artifacts to "production".
-	//if err := promote(client, version); err != nil {
-	//	return err
+func releaseTeleport(ctx context.Context, droneClient drone.Client, ghClient *github.Client, tag string, commit string) error {
+	//// Create the tags needed for the release.
+	//if err := createTags(ghClient, []string{tag}, commit); err != nil {
+	//	return trace.Wrap(err)
 	//}
 
-	// Wait for Drone to finish promoting all artifacts to "production".
-	if err := waitForTag(client, version, "production"); err != nil {
-		return err
+	//// Monitor the builds, once complete, promote.
+	//if err := build(droneClient, version); err != nil {
+	//	return trace.Wrap(err)
+	//}
+
+	//// Create the GitHub release.
+	//if err := createRelease(ghClient, version); err != nil {
+	//	return trace.Wrap(err)
+	//}
+
+	//// Create post-release PRs.
+	//if err := createPostRelease(ghClient, version); err != nil {
+	//	return trace.Wrap(err)
+	//}
+
+	return nil
+}
+
+func releaseTeleportPlugins(ctx context.Context, droneClient drone.Client, ghClient *github.Client, tag string, commit string) error {
+	fmt.Printf("Releasing Teleport Plugins: %v at %v.\n", tag, commit)
+
+	// Teleport Plugins at the moment requires the creation of multiple release
+	// tags due to how Drone performs builds. Create a tag for each plugin as
+	// well as a meta tag that will be used to create a GitHub release.
+	tags := preparePluginsTags(tag)
+
+	// Create the tags needed for the release.
+	if err := createTags(ctx, ghClient, "gh-actions-poc", tags, commit); err != nil {
+		return trace.Wrap(err)
 	}
+
+	// Monitor the builds, once complete, promote.
+	if err := build(droneClient, tags); err != nil {
+		return trace.Wrap(err)
+	}
+
+	//// Create the GitHub release.
+	//if err := createRelease(ctx, ghClient, tag); err != nil {
+	//	return trace.Wrap(err)
+	//}
+
+	fmt.Printf("Teleport Plugins successfully released.\n")
+	return nil
+}
+
+func preparePluginsTags(tag string) []string {
+	tags := make([]string, 0, len(pluginsTagPrefixes))
+	for _, prefix := range pluginsTagPrefixes {
+		tags = append(tags, fmt.Sprintf("%v-%v", prefix, tag))
+	}
+	return tags
+}
+
+func createTags(ctx context.Context, client *github.Client, repoName string, tags []string, commit string) error {
+	for _, tag := range tags {
+		_, _, err := client.Git.CreateRef(ctx, "gravitational", repoName, &github.Reference{
+			Ref: github.String(fmt.Sprintf("refs/tags/%v", tag)),
+			Object: &github.GitObject{
+				SHA: github.String(commit),
+			},
+		})
+		if err != nil {
+			return trace.Wrap(err)
+		}
+	}
+	return nil
+}
+
+func build(client drone.Client, tags []string) error {
+	//// Wait for Drone to finish building artifacts for the tag.
+	//if err := waitForTags(client, tags, ""); err != nil {
+	//	return trace.Wrap(err)
+	//}
+
+	//// Promote artifacts to "production".
+	//if err := promote(client, tags); err != nil {
+	//	return trace.Wrap(err)
+	//}
+
+	//// Wait for Drone to finish promoting all artifacts to "production".
+	//if err := waitForTags(client, tags, "production"); err != nil {
+	//	return trace.Wrap(err)
+	//}
 
 	return nil
 }
@@ -126,85 +224,169 @@ func waitForTag(client drone.Client, version string, deploy string) error {
 		}
 	}
 }
+
 func promote(client drone.Client, version string) error {
-	// Get list of active builds.
-	builds, err := client.BuildList("gravitational", "teleport-plugins", drone.ListOptions{
-		Page: 0,
-		Size: 40,
-	})
-	if err != nil {
-		return err
-	}
+	//// Get list of active builds.
+	//builds, err := client.BuildList("gravitational", "teleport-plugins", drone.ListOptions{
+	//	Page: 0,
+	//	Size: 40,
+	//})
+	//if err != nil {
+	//	return err
+	//}
 
-	for _, release := range releaseList {
-		// Find matching tag that has finished building.
-		build, err := matchBuild(fmt.Sprintf("%v-v%v", release, version), "", builds)
-		if err != nil {
-			return err
-		}
+	//for _, release := range releaseList {
+	//	// Find matching tag that has finished building.
+	//	build, err := matchBuild(fmt.Sprintf("%v-v%v", release, version), "", builds)
+	//	if err != nil {
+	//		return err
+	//	}
 
-		// Promote tag to "production".
-		_, err = client.Promote("gravitational", "teleport-plugins", int(build.Number), "production", nil)
-		if err != nil {
-			return err
-		}
-	}
+	//	// Promote tag to "production".
+	//	_, err = client.Promote("gravitational", "teleport-plugins", int(build.Number), "production", nil)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
 
 	return nil
 }
 
 func checkBuilds(builds []*drone.Build, version string, deploy string) error {
-	for _, release := range releaseList {
-		if _, err := matchBuild(fmt.Sprintf("%v-v%v", release, version), deploy, builds); err != nil {
-			return err
-		}
-	}
+	//for _, release := range releaseList {
+	//	if _, err := matchBuild(fmt.Sprintf("%v-v%v", release, version), deploy, builds); err != nil {
+	//		return err
+	//	}
+	//}
 	return nil
 }
 
 func matchBuild(name string, deploy string, builds []*drone.Build) (*drone.Build, error) {
-	for _, build := range builds {
-		if strings.HasSuffix(build.Ref, name) &&
-			build.Deploy == deploy &&
-			build.Status == "success" {
-			//log.Printf("Build %v ready: deploy=%q ref=%q.", build.Number, build.Deploy, build.Ref)
-			return build, nil
-		}
-	}
+	//for _, build := range builds {
+	//	if strings.HasSuffix(build.Ref, name) &&
+	//		build.Deploy == deploy &&
+	//		build.Status == "success" {
+	//		//log.Printf("Build %v ready: deploy=%q ref=%q.", build.Number, build.Deploy, build.Ref)
+	//		return build, nil
+	//	}
+	//}
 	return nil, fmt.Errorf("failed to find build(name=%v, deploy=%v).", name)
 }
 
-func createRelease() error {
+type release struct {
+	Version string
+}
+
+func createRelease(ctx context.Context, client *github.Client, tag string) error {
+	// Check the tag to figure out if this is a prerelease or a production release.
+	var prerelease bool
+	releaseNotes := pluginsReleaseNotes
+	if semver.Prerelease(tag) != "" {
+		prerelease = true
+		releaseNotes = prereleaseNotes
+	}
+
+	// Build out the release notes and inject the version wherever appropriate.
+	var buffer bytes.Buffer
+	t, err := template.New("releaseNotes").Parse(releaseNotes)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	err = t.Execute(&buffer, &release{
+		Version: tag,
+	})
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	// Create the tag on the remote repo.
+	_, _, err = client.Repositories.CreateRelease(ctx, "gravitational", "gh-actions-poc", &github.RepositoryRelease{
+		TagName:    github.String(tag),
+		Name:       github.String(fmt.Sprintf("Teleport Plugins %v", strings.TrimPrefix(tag, "v"))),
+		Body:       github.String(strings.TrimSpace(buffer.String())),
+		Prerelease: github.Bool(prerelease),
+	})
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
 	return nil
 }
 
 func main() {
 	// Read in and parse all command line flags.
-	version, err := readFlags()
+	tag, teleportCommit, pluginsCommit, err := readFlags()
 	if err != nil {
 		log.Fatalf("Failed to parse flags: %v.", err)
 	}
+	fmt.Printf("Creating release %v for Teleport and Teleport Plugins.\n\n", tag)
 
 	// Read in credentials and create Drone and GitHub clients.
-	droneClient, ghClient, err := readCredentials()
+	droneClient, ghClient, err := buildClients()
 	if err != nil {
 		log.Fatalf("Failed to read in credentials: %v.", err)
 	}
 
-	// Create the tags needed for the release.
-	if err := createTags(ghClient, version); err != nil {
-		log.Fatalf("Failed to create tags: %v.", err)
+	if err := releaseTeleport(context.Background(), droneClient, ghClient, tag, teleportCommit); err != nil {
+		log.Fatalf("Failed to release Teleport: %v.", err)
 	}
 
-	// Monitor the builds, once complete, promote.
-	if err := build(droneClient, version); err != nil {
-		log.Fatalf("Failed to build: %v.", err)
+	if err := releaseTeleportPlugins(context.Background(), droneClient, ghClient, tag, pluginsCommit); err != nil {
+		log.Fatalf("Failed to release Teleport Plugins: %v.", err)
 	}
 
-	// Create the GitHub release.
-	if err := createRelease(ghClient, version); err != nil {
-		log.Fatalf("Failed to create release: %v.", err)
-	}
-
-	log.Printf("Successfully released telport-plugins %v.", version)
+	fmt.Printf("\nRelease %v successful.", tag)
 }
+
+var pluginsTagPrefixes = []string{
+	"teleport-event-handler",
+	"teleport-jira",
+	"teleport-mattermost",
+	"teleport-slack",
+	"teleport-pagerduty",
+	"terraform-provider-teleport",
+}
+
+type ghConfig struct {
+	Host ghHost `yaml:"github.com"`
+}
+
+type ghHost struct {
+	User     string `yaml:"user"`
+	Token    string `yaml:"oauth_token"`
+	Protocol string `yaml:"git_protocol"`
+}
+
+const (
+	// prereleaseNotes are generic release notes that are attached to all releases.
+	prereleaseNotes = `
+## Warning
+
+Pre-releases are not production ready, use at your own risk!
+
+## Download
+
+Download the current and previous releases of Teleport at https://gravitational.com/teleport/download.`
+
+	// pluginsReleaseNotes are generic release notes that are attached to all
+	// Teleport Plugins releases.
+	pluginsReleaseNotes = `
+## Description
+
+A set of plugins for Teleport's for Access Workflows.
+
+## Documentation
+
+Documentation and guides are available for [Teleport Access Requests](https://goteleport.com/docs/enterprise/workflow), [Terraform Provider](https://goteleport.com/docs/setup/guides/terraform-provider), and [Event Handler](https://goteleport.com/docs/setup/guides/fluentd).
+
+## Download
+
+Download the current release from the links below.
+
+* [Slack](https://get.gravitational.com/teleport-access-slack-{{.Version}}-linux-amd64-bin.tar.gz)
+* [Mattermost](https://get.gravitational.com/teleport-access-mattermost-{{.Version}}-linux-amd64-bin.tar.gz)
+* [Terraform Provider](https://get.gravitational.com/terraform-provider-teleport-{{.Version}}-linux-amd64-bin.tar.gz)
+* [Event Handler](https://get.gravitational.com/teleport-event-handler-{{.Version}}-linux-amd64-bin.tar.gz)
+* [PagerDuty](https://get.gravitational.com/teleport-access-pagerduty-{{.Version}}-linux-amd64-bin.tar.gz)
+* [Jira](https://get.gravitational.com/teleport-access-jira-{{.Version}}-linux-amd64-bin.tar.gz)`
+)
