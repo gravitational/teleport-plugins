dc30ca40368b0120436d33a90310da8686e60408
Victor Sokolov <gzigzigzeo@gmail.com>
2021-08-27 00:43:28 +0300

    Fixes SAML and Github connectors schema

---
+++ terraform/example/oidc_connector.tf
@@ -0,0 +1,19 @@
+# Teleport OIDC connector
+
+# resource "teleport_oidc_connector" "oidc" {
+#   metadata {
+#      name = "test"
+#      labels = {
+#        test = "yes"
+#      }
+#   }
+#   spec {
+#     client_id = "client"
+#     client_secret = "value"
+
+#     claims_to_roles {
+#       claim = "test"
+#       roles = ["terraform"]
+#     }
+#   }
+# }
