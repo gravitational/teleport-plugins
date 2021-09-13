45aefbbc2aee24053962060aab04d14aea5b0130
ReeceCrossland <10209831+ReeceCrossland@users.noreply.github.com>
2021-09-13 10:19:26 +0100

    Obscure part of token to make it a sensitive resource

--- terraform/provider/resource_teleport_provision_token.go
+++ terraform/provider/resource_teleport_provision_token.go
@@ -21,12 +21,126 @@ import (
 	"fmt"
 
 	"github.com/gravitational/teleport-plugins/terraform/tfschema"
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
-	"github.com/gravitational/teleport/api/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
+
+func GenSchemaProvisionTokenV2() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		// Kind is a resource kind
+		"kind": {
+			Type:        schema.TypeString,
+			Description: "Kind is a resource kind",
+			Optional:    true,
+			Default:     "token",
+		},
+		// SubKind is an optional resource sub kind, used in some resources
+		"sub_kind": {
+			Type:        schema.TypeString,
+			Description: "SubKind is an optional resource sub kind, used in some resources",
+			Optional:    true,
+			Default:     "",
+		},
+		// Version is version
+		"version": {
+			Type:        schema.TypeString,
+			Description: "Version is version",
+			Optional:    true,
+			Default:     "v2",
+		},
+		"index": {
+			Type:        schema.TypeString,
+			Description: "TODO",
+			Required:    true,
+			ForceNew:    true,
+		},
+		"name": {
+			Type:        schema.TypeString,
+			Description: "TODO",
+			Required:    true,
+			ForceNew:    true,
+			Sensitive:   true,
+		},
+		// Metadata is resource metadata
+		"metadata": {
+			Type:        schema.TypeList,
+			MaxItems:    1,
+			Description: "Metadata is resource metadata",
+			Optional:    true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					// Name is an object name
+					"name": {
+						Type:        schema.TypeString,
+						Description: "Name is an object name",
+						Sensitive:   true,
+						Computed:    true,
+					},
+					// Namespace is object namespace. The field should be called "namespace"
+					// when it returns in Teleport 2.4.
+					"namespace": {
+						Type:        schema.TypeString,
+						Description: "Namespace is object namespace. The field should be called \"namespace\"  when it returns in Teleport 2.4.",
+						Optional:    true,
+						Default:     "default",
+					},
+					// Description is object description
+					"description": {
+						Type:        schema.TypeString,
+						Description: "Description is object description",
+						Optional:    true,
+					},
+					// Labels is a set of labels
+					"labels": {
+
+						Optional:    true,
+						Type:        schema.TypeMap,
+						Description: "Labels is a set of labels",
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
+						},
+					},
+					// Expires is a global expiry time header can be set on any resource in the
+					// system.
+					"expires": {
+						Type:         schema.TypeString,
+						Description:  "Expires is a global expiry time header can be set on any resource in the  system.",
+						ValidateFunc: validation.IsRFC3339Time,
+						StateFunc:    tfschema.TruncateMs,
+						Required:     true,
+					},
+				},
+			},
+		},
+		// Spec is a provisioning token V2 spec
+		"spec": {
+			Type:        schema.TypeList,
+			MaxItems:    1,
+			Description: "ProvisionTokenSpecV2 is a specification for V2 token",
+
+			Required: true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					// Roles is a list of roles associated with the token,
+					// that will be converted to metadata in the SSH and X509
+					// certificates issued to the user of the token
+					"roles": {
+
+						Required:    true,
+						Type:        schema.TypeList,
+						Description: "Roles is a list of roles associated with the token,  that will be converted to metadata in the SSH and X509  certificates issued to the user of the token",
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
+						},
+					},
+				},
+			},
+		},
+	}
+}
 
 // resourceTeleportProvisionToken returns Teleport github_connector resource definition
 func resourceTeleportProvisionToken() *schema.Resource {
@@ -36,7 +150,7 @@ func resourceTeleportProvisionToken() *schema.Resource {
 		UpdateContext: resourceProvisionTokenUpdate,
 		DeleteContext: resourceProvisionTokenDelete,
 
-		Schema:        tfschema.SchemaProvisionTokenV2,
+		Schema:        GenSchemaProvisionTokenV2(),
 		SchemaVersion: 2,
 
 		Importer: &schema.ResourceImporter{
@@ -52,16 +166,34 @@ func resourceProvisionTokenCreate(ctx context.Context, d *schema.ResourceData, m
 		return diagFromErr(err)
 	}
 
-	name, err := getResourceName(d, "provision_token")
-	if err != nil {
-		return diagFromErr(err)
+	index, ok := d.GetOk("index")
+	if !ok {
+		return diagFromErr(fmt.Errorf("Index is required"))
+	}
+	name, ok := d.GetOk("name")
+	if !ok {
+		return diagFromErr(fmt.Errorf("Name is required"))
+	}
+
+	token := index.(string) + name.(string)
+	metadata, ok := d.GetOk("metadata")
+	if !ok {
+		md := []interface{}{}
+		md0 := map[string]interface{}{"name": token}
+		md = append(md, md0)
+		d.Set("metadata", md)
+	} else {
+		md := metadata.([]interface{})
+		md0 := md[0].(map[string]interface{})
+		md0["name"] = token
+		d.Set("metadata", md)
 	}
 
 	// Check if token already exists
-	_, err = c.GetToken(ctx, name)
+	_, err = c.GetToken(ctx, token)
 	if err == nil {
-		existErr := "token " + name + " exists in Teleport. Either remove it (tctl tokens rm " + name + ")" +
-			" or import it to the existing state (terraform import teleport_provision_token." + name + " " + name + ")"
+		existErr := "token " + token + " exists in Teleport. Either remove it (tctl tokens rm " + token + ")" +
+			" or import it to the existing state (terraform import teleport_provision_token." + index.(string) + " " + token + ")"
 
 		return diagFromErr(trace.Errorf(existErr))
 	}
@@ -77,7 +209,7 @@ func resourceProvisionTokenCreate(ctx context.Context, d *schema.ResourceData, m
 	}
 
 	// Create and validate token
-	t, err := types.NewProvisionToken(name, tmp.Spec.Roles, tmp.Metadata.Expiry())
+	t, err := types.NewProvisionToken(token, tmp.Spec.Roles, tmp.Metadata.Expiry())
 	if err != nil {
 		return diagFromErr(err)
 	}
@@ -94,11 +226,12 @@ func resourceProvisionTokenCreate(ctx context.Context, d *schema.ResourceData, m
 	}
 
 	err = c.UpsertToken(ctx, tV2)
+
 	if err != nil {
-		return diagFromErr(describeErr(err, "token"))
+		return diagFromErr(fmt.Errorf("API Error: %v", err))
 	}
 
-	d.SetId(tV2.GetName())
+	d.SetId(index.(string))
 
 	return resourceProvisionTokenRead(ctx, d, m)
 }
@@ -110,9 +243,12 @@ func resourceProvisionTokenRead(ctx context.Context, d *schema.ResourceData, m i
 		return diagFromErr(err)
 	}
 
-	id := d.Id()
+	index := d.Id()
+	name, ok := d.GetOk("name")
 
-	t, err := c.GetToken(ctx, id)
+	token := index + name.(string)
+
+	t, err := c.GetToken(ctx, token)
 	if trace.IsNotFound(err) {
 		d.SetId("")
 		return diag.Diagnostics{}
@@ -142,10 +278,25 @@ func resourceProvisionTokenUpdate(ctx context.Context, d *schema.ResourceData, m
 		return diagFromErr(err)
 	}
 
-	id := d.Id()
+	index := d.Id()
+	name, ok := d.GetOk("name")
+
+	token := index + name.(string)
+	metadata, ok := d.GetOk("metadata")
+	if !ok {
+		md := []interface{}{}
+		md0 := map[string]interface{}{"name": token}
+		md = append(md, md0)
+		d.Set("metadata", md)
+	} else {
+		md := metadata.([]interface{})
+		md0 := md[0].(map[string]interface{})
+		md0["name"] = token
+		d.Set("metadata", md)
+	}
 
 	// Check that token exists. This situation is hardly possible because Terraform tries to read a resource before updating it.
-	t, err := c.GetToken(ctx, id)
+	t, err := c.GetToken(ctx, token)
 	if err != nil {
 		return diagFromErr(describeErr(err, "token"))
 	}
@@ -175,8 +326,14 @@ func resourceProvisionTokenDelete(ctx context.Context, d *schema.ResourceData, m
 		return diagFromErr(err)
 	}
 
-	id := d.Id()
-	err = c.DeleteToken(ctx, id)
+	index := d.Id()
+	name, ok := d.GetOk("name")
+
+	if !ok {
+		return diagFromErr(fmt.Errorf("error"))
+	}
+	token := index + name.(string)
+	err = c.DeleteToken(ctx, token)
 	if err != nil {
 		return diagFromErr(describeErr(err, "token"))
 	}
