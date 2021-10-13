49af0ee9ce5da62d3885cac67c5c7868e5dce3d3
Brian Joerger <bjoerger@gravitational.com>
2021-10-13 16:22:19 -0700

    Apply suggestions from code review

--- lib/backoff/decorr.go
+++ lib/backoff/decorr.go
@@ -35,12 +35,12 @@ type decorr struct {
 	clock clockwork.Clock
 }
 
-// NewDecorr initializes an alogorithm.
+// NewDecorr initializes an algorithm.
 func NewDecorr(base, cap time.Duration, clock clockwork.Clock) Backoff {
 	return NewDecorrWithMul(base, cap, 3, clock)
 }
 
-// NewDecorrWithMul initializes a backoff alogorithm with a give multiplier.
+// NewDecorrWithMul initializes a backoff algorithm with a given multiplier.
 func NewDecorrWithMul(base, cap time.Duration, mul int64, clock clockwork.Clock) Backoff {
 	return &decorr{
 		base:  int64(base),
