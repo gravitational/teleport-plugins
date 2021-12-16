39d55ae8133c98473d8d7841f65dcc0d9ec4bc98
Edoardo Spadolini <edoardo.spadolini@gmail.com>
2021-12-16 11:46:35 +0100

    Use net/mail to check for email address validity (#396)

--- lib/email.go
+++ lib/email.go
@@ -1,9 +1,11 @@
 package lib
 
-import "regexp"
-
-var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+import "net/mail"
 
 func IsEmail(str string) bool {
-	return emailRegex.MatchString(str)
+	address, err := mail.ParseAddress(str)
+	if err != nil {
+		return false
+	}
+	return str == address.Address
 }
