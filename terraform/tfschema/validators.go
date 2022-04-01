2fe0d09ee43186d84b0c4a004cd6ed2bdf9a16ae
Marco Dinis <marco.dinis@goteleport.com>
2022-04-01 14:56:51 +0100

    Add OneOf validator for Object terraform types

--- terraform/tfschema/validators.go
+++ terraform/tfschema/validators.go
@@ -19,6 +19,7 @@ package tfschema
 import (
 	"context"
 	fmt "fmt"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
@@ -162,4 +163,68 @@ OUTER:
 		resp.Diagnostics.AddError("Map keys validation error", fmt.Sprintf("Key %v must be present in the map %v", k, req.AttributePath.String()))
 	}
 
+}
+
+// OneOfValidator validates that at least one of the attributes is set (known and not null)
+type OneOfValidator struct {
+	Keys []string
+}
+
+// UseOneOfValidator creates OneOfValidator
+func UseOneOfValidator(keys ...string) tfsdk.AttributeValidator {
+	return OneOfValidator{
+		Keys: keys,
+	}
+}
+
+// Description returns validator description
+func (v OneOfValidator) Description(_ context.Context) string {
+	return fmt.Sprintf("OneOf '%s' attributes must be present", strings.Join(v.Keys, ", "))
+}
+
+// MarkdownDescription returns validator markdown description
+func (v OneOfValidator) MarkdownDescription(_ context.Context) string {
+	return fmt.Sprintf("OneOf `%s` attributes must be present", strings.Join(v.Keys, ", "))
+}
+
+// Validate performs the validation.
+func (v OneOfValidator) Validate(ctx context.Context, req tfsdk.ValidateAttributeRequest, resp *tfsdk.ValidateAttributeResponse) {
+	if req.AttributeConfig == nil {
+		return
+	}
+
+	value, ok := req.AttributeConfig.(types.Object)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"OneOf Object validation error",
+			fmt.Sprintf("Attribute %v can not be converted to Object", req.AttributePath.String()),
+		)
+		return
+	}
+
+	if value.Null || value.Unknown {
+		return
+	}
+
+	for _, key := range v.Keys {
+		attr, found := value.Attrs[key]
+		if found {
+			tfVal, err := attr.ToTerraformValue(ctx)
+			if err != nil {
+				resp.Diagnostics.AddError(
+					"OneOf keys validation error",
+					fmt.Sprintf("Failed to convert ToTerraformValue attribute with key '%s': %v", key, err),
+				)
+				return
+			}
+			if !tfVal.IsNull() {
+				return
+			}
+		}
+	}
+
+	resp.Diagnostics.AddError(
+		"OneOf keys validation error",
+		fmt.Sprintf("OneOf '%s' keys must be present", strings.Join(v.Keys, ", ")),
+	)
 }
