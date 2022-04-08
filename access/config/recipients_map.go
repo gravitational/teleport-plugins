65a27476a688aad4d4466e8fcfdf6bd53f96b7a4
Marco Dinis <marco.dinis@goteleport.com>
2022-04-08 09:26:28 +0100

    add godoc and license headers

--- access/config/recipients_map.go
+++ access/config/recipients_map.go
@@ -1,3 +1,18 @@
+/*
+   Copyright 2022 Gravitational, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package config
 
 import "fmt"
@@ -5,6 +20,10 @@ import "fmt"
 // RecipientsMap is a mapping of roles to recipient(s).
 type RecipientsMap map[string][]string
 
+// UnmarshalTOML will convert the input into map[string][]string
+// The input can be one of the following:
+// "key" = "value"
+// "key" = ["multiple", "values"]
 func (r *RecipientsMap) UnmarshalTOML(in interface{}) error {
 	*r = make(RecipientsMap)
 
