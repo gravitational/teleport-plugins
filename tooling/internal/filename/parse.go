13de69776b34e26c510f99c28385f808abecad4e
Trent Clarke <trent@goteleport.com>
2022-05-05 10:48:53 +1000

    Backfill licenses

--- tooling/internal/filename/parse.go
+++ tooling/internal/filename/parse.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2022 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package filename
 
 import (
@@ -17,13 +32,13 @@ var (
 // filename.
 type Info struct {
 	// Type represents the plugin type, e.g. "terraform-provider"
-	Type    string
+	Type string
 	// Version holds the parsed plugin version number
 	Version semver.Version
 	// OS is the operating system the plugin was built for
-	OS      string
+	OS string
 	// Arch is the CPU architecture the plugin was built for
-	Arch    string
+	Arch string
 }
 
 // Parse attempts to deduce information about a staged plugin from its (assumed
