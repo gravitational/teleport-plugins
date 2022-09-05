c1dcf4751d5ebe5c0cf406884b718dc4b0019ca5
Marco Dinis <marco.dinis@goteleport.com>
2022-09-05 16:23:05 +0100

    it fails

---
+++ terraform/test/fixtures/role_drift_0.tf
@@ -0,0 +1,13 @@
+resource "teleport_role" "splitbrain" {
+    metadata = {
+        name = "splitbrain"
+    }
+
+    spec = {
+        allow = {
+            logins = ["one"]
+        }
+    }
+
+    version = "v5"
+}
