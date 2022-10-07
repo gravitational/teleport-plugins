b0f2ce8aec89457bb03cb576ff6d362a0a890fa9
Hugo Shaka <hugo.hervieux@goteleport.com>
2022-10-07 11:20:56 -0400

    Fail early if the credentials are expired (#672)
    This PR introduces a credential check on plugin startup.
    Failing fast with a meaningful error message is a better user experience than
    hanging for several seconds and returning cryptic errors.

--- access/mattermost/app.go
+++ access/mattermost/app.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gravitational/teleport-plugins/lib"
 	"github.com/gravitational/teleport-plugins/lib/backoff"
+	"github.com/gravitational/teleport-plugins/lib/credentials"
 	"github.com/gravitational/teleport-plugins/lib/logger"
 	"github.com/gravitational/teleport-plugins/lib/stringset"
 	"github.com/gravitational/teleport-plugins/lib/watcherjob"
@@ -113,6 +114,16 @@ func (a *App) init(ctx context.Context) error {
 	ctx, cancel := context.WithTimeout(ctx, initTimeout)
 	defer cancel()
 	log := logger.Get(ctx)
+
+	if validCred, err := credentials.CheckIfExpired(a.conf.Teleport.Credentials()); err != nil {
+		log.Warn(err)
+		if !validCred {
+			return trace.BadParameter(
+				"No valid credentials found, this likely means credentials are expired. In this case, please sign new credentials and increase their TTL if needed.",
+			)
+		}
+		log.Info("At least one non-expired credential has been found, continuing startup")
+	}
 
 	var (
 		err  error
