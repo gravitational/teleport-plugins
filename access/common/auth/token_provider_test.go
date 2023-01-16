bbface3fff5251a61791e3ce64060f8ccf85e1d6
Justinas Stankevicius <justinas@goteleport.com>
2023-01-16 22:33:57 +0200

    Port token_provider_test to vanilla testing package

--- access/common/auth/token_provider_test.go
+++ access/common/auth/token_provider_test.go
@@ -5,11 +5,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gravitational/teleport-plugins/access/common/auth/oauth"
 	"github.com/gravitational/teleport-plugins/access/common/auth/state"
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
 	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/require"
 )
 
 type mockRefresher struct {
@@ -36,134 +37,140 @@ func (s *mockState) PutCredentials(ctx context.Context, creds *state.Credentials
 	return s.putCredentials(creds)
 }
 
-type RotatedAccessTokenProviderSuite struct {
-	suite.Suite
-
-	refresher *mockRefresher
-	state     *mockState
-	clock     clockwork.FakeClock
-	log       *logrus.Entry
-
-	initialCreds *state.Credentials
-}
-
-func (r *RotatedAccessTokenProviderSuite) SetupSuite() {
-	r.refresher = &mockRefresher{}
-	r.state = &mockState{}
-	r.clock = clockwork.NewFakeClock()
+func TestRotatedAccessTokenProvider(t *testing.T) {
 	log := logrus.New()
 	log.Level = logrus.DebugLevel
-	r.log = logrus.NewEntry(log)
 
-	r.initialCreds = &state.Credentials{
-		AccessToken:  "my-access-token",
-		RefreshToken: "my-refresh-token",
-		ExpiresAt:    r.clock.Now().Add(2 * time.Hour),
-	}
-}
+	newProvider := func(ctx context.Context, state state.State, refresher oauth.Refresher, clock clockwork.Clock, initialCreds *state.Credentials) *RotatedAccessTokenProvider {
+		return &RotatedAccessTokenProvider{
+			ctx:       ctx,
+			state:     state,
+			refresher: refresher,
+			clock:     clock,
 
-func (r *RotatedAccessTokenProviderSuite) newProvider(ctx context.Context) *RotatedAccessTokenProvider {
-	return &RotatedAccessTokenProvider{
-		ctx:       ctx,
-		state:     r.state,
-		refresher: r.refresher,
-		clock:     r.clock,
+			retryInterval:       1 * time.Minute,
+			tokenBufferInterval: 1 * time.Hour,
 
-		retryInterval:       1 * time.Minute,
-		tokenBufferInterval: 1 * time.Hour,
-
-		creds: r.initialCreds,
-		log:   r.log,
+			creds: initialCreds,
+			log:   log,
+		}
 	}
 
-}
+	t.Run("Init", func(t *testing.T) {
+		clock := clockwork.NewFakeClock()
+		initialCreds := &state.Credentials{
+			AccessToken:  "my-access-token",
+			RefreshToken: "my-refresh-token",
+			ExpiresAt:    clock.Now().Add(2 * time.Hour),
+		}
 
-func (r *RotatedAccessTokenProviderSuite) TestInit() {
-	r.state.getCredentials = func() (*state.Credentials, error) {
-		return r.initialCreds, nil
-	}
+		refresher := &mockRefresher{}
+		mockState := &mockState{
+			getCredentials: func() (*state.Credentials, error) {
+				return initialCreds, nil
+			},
+		}
 
-	provider, err := NewRotatedTokenProvider(RotatedAccessTokenProviderConfig{
-		Ctx:       context.Background(),
-		State:     r.state,
-		Refresher: r.refresher,
-		Clock:     r.clock,
+		provider, err := NewRotatedTokenProvider(RotatedAccessTokenProviderConfig{
+			Ctx:       context.Background(),
+			State:     mockState,
+			Refresher: refresher,
+			Clock:     clock,
+		})
+		require.NoError(t, err)
+		creds, err := provider.GetAccessToken()
+		require.NoError(t, err)
+		require.Equal(t, initialCreds.AccessToken, creds)
 	})
-	r.Require().NoError(err)
-	creds, err := provider.GetAccessToken()
-	r.Require().NoError(err)
-	r.Require().Equal(r.initialCreds.AccessToken, creds)
-}
 
-func (r *RotatedAccessTokenProviderSuite) TestRefresh() {
-	var storedCreds *state.Credentials
-
-	newCreds := &state.Credentials{
-		AccessToken:  "my-access-token2",
-		RefreshToken: "my-refresh-token2",
-		ExpiresAt:    r.clock.Now().Add(4 * time.Hour),
-	}
-
-	r.state.getCredentials = func() (*state.Credentials, error) {
-		return r.initialCreds, nil
-	}
-	r.state.putCredentials = func(creds *state.Credentials) error {
-		storedCreds = creds
-		return nil
-	}
-
-	var refreshCalled int
-	r.refresher.refresh = func(refreshToken string) (*state.Credentials, error) {
-		refreshCalled++
-		r.Require().Equal(refreshToken, r.initialCreds.RefreshToken)
-
-		// fail the first call
-		if refreshCalled == 1 {
-			return nil, trace.Errorf("some error")
+	t.Run("Refresh", func(t *testing.T) {
+		clock := clockwork.NewFakeClock()
+		initialCreds := &state.Credentials{
+			AccessToken:  "my-access-token",
+			RefreshToken: "my-refresh-token",
+			ExpiresAt:    clock.Now().Add(2 * time.Hour),
+		}
+		newCreds := &state.Credentials{
+			AccessToken:  "my-access-token2",
+			RefreshToken: "my-refresh-token2",
+			ExpiresAt:    clock.Now().Add(4 * time.Hour),
 		}
 
-		return newCreds, nil
-	}
+		var storedCreds *state.Credentials
+		var refreshCalled int
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+		refresher := &mockRefresher{
+			refresh: func(refreshToken string) (*state.Credentials, error) {
+				refreshCalled++
+				require.Equal(t, refreshToken, initialCreds.RefreshToken)
 
-	provider := r.newProvider(ctx)
+				// fail the first call
+				if refreshCalled == 1 {
+					return nil, trace.Errorf("some error")
+				}
 
-	go provider.RefreshLoop()
-
-	r.clock.BlockUntil(1)
-	r.Require().Nil(storedCreds) // before attempting refresh
-
-	r.clock.Advance(1 * time.Hour) // trigger refresh (2 hours - 1 hour buffer)
-	r.clock.BlockUntil(1)
-	r.Require().Nil(storedCreds) // after first refresh has failed
-
-	r.clock.Advance(1 * time.Minute) // trigger refresh (after retry period)
-	r.clock.BlockUntil(1)
-	r.Require().Equal(newCreds, storedCreds)
-}
-
-func (r *RotatedAccessTokenProviderSuite) TestCancel() {
-	finished := make(chan struct{}, 1)
-	ctx, cancel := context.WithCancel(context.Background())
-
-	provider := r.newProvider(ctx)
-
-	go func() {
-		provider.RefreshLoop()
-		finished <- struct{}{}
-	}()
-
-	cancel()
-	r.Require().Eventually(func() bool {
-		select {
-		case <-finished:
-			return true
-		default:
-			return false
+				return newCreds, nil
+			},
 		}
-	}, time.Second, time.Second/10)
-}
+		mockState := &mockState{
+			getCredentials: func() (*state.Credentials, error) {
+				return initialCreds, nil
+			},
+			putCredentials: func(creds *state.Credentials) error {
+				storedCreds = creds
+				return nil
+			},
+		}
 
-func TestRotatedAccessTokenProvider(t *testing.T) { suite.Run(t, &RotatedAccessTokenProviderSuite{}) }
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		provider := newProvider(ctx, mockState, refresher, clock, initialCreds)
+
+		go provider.RefreshLoop()
+
+		clock.BlockUntil(1)
+		require.Nil(t, storedCreds) // before attempting refresh
+
+		clock.Advance(1 * time.Hour) // trigger refresh (2 hours - 1 hour buffer)
+		clock.BlockUntil(1)
+		require.Nil(t, storedCreds) // after first refresh has failed
+
+		clock.Advance(1 * time.Minute) // trigger refresh (after retry period)
+		clock.BlockUntil(1)
+		require.Equal(t, newCreds, storedCreds)
+	})
+
+	t.Run("Cancel", func(t *testing.T) {
+		clock := clockwork.NewFakeClock()
+		refresher := &mockRefresher{}
+		mockState := &mockState{}
+
+		initialCreds := &state.Credentials{
+			AccessToken:  "my-access-token",
+			RefreshToken: "my-refresh-token",
+			ExpiresAt:    clock.Now().Add(2 * time.Hour),
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		provider := newProvider(ctx, mockState, refresher, clock, initialCreds)
+		finished := make(chan struct{}, 1)
+
+		go func() {
+			provider.RefreshLoop()
+			finished <- struct{}{}
+		}()
+
+		cancel()
+		require.Eventually(t, func() bool {
+			select {
+			case <-finished:
+				return true
+			default:
+				return false
+			}
+		}, time.Second, time.Second/10)
+	})
+}
