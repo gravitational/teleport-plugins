0654db5cd54c498fd4bc5daa2b2279aeb40f9e04
Justinas Stankevicius <justinas@goteleport.com>
2023-01-17 18:51:20 +0200

    Port oauth_test to vanilla testing package

--- access/slack/oauth_test.go
+++ access/slack/oauth_test.go
@@ -1,152 +1,169 @@
 package slack
 
 import (
+	"context"
 	"encoding/json"
-	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
-	"github.com/gravitational/teleport-plugins/lib/testing/integration"
 	"github.com/julienschmidt/httprouter"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/require"
 )
 
-type OAuthSuite struct {
-	integration.Suite
-
-	// Request parameters
+type testOAuthServer struct {
 	clientID          string
 	clientSecret      string
 	authorizationCode string
 	redirectURI       string
 	refreshToken      string
 
-	// Response parameters
-	exchangedAccessToken  string
-	exchangedRefreshToken string
-	refreshedAccessToken  string
-	refreshedRefreshToken string
-	expiresInSeconds      int
+	exchangeResponse AccessResponse
+	refreshResponse  AccessResponse
 
-	exchangeError error
-	refreshError  error
-
-	srv        *httptest.Server
-	authorizer *Authorizer
+	srv *httptest.Server
+	t   *testing.T
 }
 
-func (s *OAuthSuite) SetupSuite() {
-	s.clientID = "my-client-id"
-	s.clientSecret = "my-client-secret"
-	s.authorizationCode = "12345678"
-	s.redirectURI = "https://foobar.com/callback"
-	s.refreshToken = "my-refresh-token1"
-	s.exchangedAccessToken = "my-access-token1"
-	s.exchangedRefreshToken = "my-refresh-token2"
-	s.refreshedAccessToken = "my-access-token2"
-	s.refreshedRefreshToken = "my-refresh-token3"
-	s.expiresInSeconds = 43200
-
-	exchange := func(w http.ResponseWriter, r *http.Request) {
-		q := r.URL.Query()
-		s.Require().Equal(s.clientID, q.Get("client_id"))
-		s.Require().Equal(s.clientSecret, q.Get("client_secret"))
-		s.Require().Equal(s.redirectURI, q.Get("redirect_uri"))
-		s.Require().Equal(s.authorizationCode, q.Get("code"))
-
-		resp := AccessResponse{
-			APIResponse: APIResponse{Ok: true},
-		}
-		if s.exchangeError != nil {
-			resp.Ok = false
-			resp.Error = s.exchangeError.Error()
-		} else {
-			resp.AccessToken = s.exchangedAccessToken
-			resp.RefreshToken = s.exchangedRefreshToken
-			resp.ExpiresInSeconds = s.expiresInSeconds
-		}
-
-		w.Header().Add("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode(resp)
-		s.Require().NoError(err)
+func (s *testOAuthServer) handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if grantType := r.URL.Query().Get("grant_type"); grantType == "refresh_token" {
+		s.refresh(w, r)
+	} else {
+		s.exchange(w, r)
 	}
+}
 
-	refresh := func(w http.ResponseWriter, r *http.Request) {
-		q := r.URL.Query()
-		s.Require().Equal(s.clientID, q.Get("client_id"))
-		s.Require().Equal(s.clientSecret, q.Get("client_secret"))
-		s.Require().Equal(s.refreshToken, q.Get("refresh_token"))
+func (s *testOAuthServer) exchange(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	require.Equal(s.t, s.clientID, q.Get("client_id"))
+	require.Equal(s.t, s.clientSecret, q.Get("client_secret"))
+	require.Equal(s.t, s.redirectURI, q.Get("redirect_uri"))
+	require.Equal(s.t, s.authorizationCode, q.Get("code"))
 
-		resp := AccessResponse{
-			APIResponse: APIResponse{Ok: true},
-		}
-		if s.refreshError != nil {
-			resp.Ok = false
-			resp.Error = s.refreshError.Error()
-		} else {
-			resp.AccessToken = s.refreshedAccessToken
-			resp.RefreshToken = s.refreshedRefreshToken
-			resp.ExpiresInSeconds = s.expiresInSeconds
-		}
+	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(s.exchangeResponse)
+	require.NoError(s.t, err)
+}
 
-		w.Header().Add("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode(resp)
-		s.Require().NoError(err)
-	}
+func (s *testOAuthServer) refresh(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	require.Equal(s.t, s.clientID, q.Get("client_id"))
+	require.Equal(s.t, s.clientSecret, q.Get("client_secret"))
+	require.Equal(s.t, s.refreshToken, q.Get("refresh_token"))
 
-	handler := func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		if grantType := r.URL.Query().Get("grant_type"); grantType == "refresh_token" {
-			refresh(w, r)
-		} else {
-			exchange(w, r)
-		}
-	}
+	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(s.refreshResponse)
+	require.NoError(s.t, err)
+}
+
+func (s *testOAuthServer) start() {
 	router := httprouter.New()
-	router.POST("/oauth.v2.access", handler)
+	router.POST("/oauth.v2.access", s.handler)
 
 	s.srv = httptest.NewServer(router)
-	s.authorizer = newAuthorizer(makeSlackClient(s.srv.URL+"/"), s.clientID, s.clientSecret)
 }
 
-func (s *OAuthSuite) SetupTest() {
-	s.exchangeError = nil
-	s.refreshError = nil
+func (s *testOAuthServer) url() string {
+	return s.srv.URL + "/"
 }
 
-func (s *OAuthSuite) TearDownSuite() {
+func (s *testOAuthServer) close() {
 	s.srv.Close()
 }
 
-func (s *OAuthSuite) TestExchangeOK() {
-	creds, err := s.authorizer.Exchange(s.Context(), s.authorizationCode, s.redirectURI)
-	s.Require().NoError(err)
-	s.Require().Equal(s.exchangedAccessToken, creds.AccessToken)
-	s.Require().Equal(s.exchangedRefreshToken, creds.RefreshToken)
-	s.Require().WithinDuration(time.Now().Add(time.Duration(s.expiresInSeconds)*time.Second), creds.ExpiresAt, 1*time.Second)
-}
+func TestOAuth(t *testing.T) {
+	const clientID = "my-client-id"
+	const clientSecret = "my-client-secret"
+	const authorizationCode = "12345678"
+	const redirectURI = "https://foobar.com/callback"
+	const refreshToken = "my-refresh-token1"
 
-func (s *OAuthSuite) TestExchangeError() {
-	s.exchangeError = errors.New("invalid_code")
-	_, err := s.authorizer.Exchange(s.Context(), s.authorizationCode, s.redirectURI)
-	s.Require().Error(err)
-	s.Require().ErrorContains(err, "invalid_code")
-}
+	const expiresInSeconds = 43200
 
-func (s *OAuthSuite) TestRefreshOK() {
-	creds, err := s.authorizer.Refresh(s.Context(), s.refreshToken)
-	s.Require().NoError(err)
-	s.Require().Equal(s.refreshedAccessToken, creds.AccessToken)
-	s.Require().Equal(s.refreshedRefreshToken, creds.RefreshToken)
-	s.Require().WithinDuration(time.Now().Add(time.Duration(s.expiresInSeconds)*time.Second), creds.ExpiresAt, 1*time.Second)
-}
+	newServer := func(t *testing.T) *testOAuthServer {
+		s := &testOAuthServer{
+			clientID:          clientID,
+			clientSecret:      clientSecret,
+			authorizationCode: authorizationCode,
+			redirectURI:       redirectURI,
+			refreshToken:      refreshToken,
 
-func (s *OAuthSuite) TestRefreshError() {
-	s.refreshError = errors.New("expired_token")
-	_, err := s.authorizer.Refresh(s.Context(), s.refreshToken)
-	s.Require().Error(err)
-	s.Require().ErrorContains(err, "expired_token")
-}
+			t: t,
+		}
+		s.start()
+		return s
+	}
 
-func TestOAuth(t *testing.T) { suite.Run(t, &OAuthSuite{}) }
+	ok := func(accessToken string, refreshToken string, expiresInSeconds int) AccessResponse {
+		return AccessResponse{
+			APIResponse:      APIResponse{Ok: true},
+			AccessToken:      accessToken,
+			RefreshToken:     refreshToken,
+			ExpiresInSeconds: expiresInSeconds,
+		}
+	}
+
+	fail := func(e string) AccessResponse {
+		return AccessResponse{
+			APIResponse: APIResponse{
+				Ok:    false,
+				Error: e,
+			},
+		}
+	}
+
+	t.Run("ExchangeOK", func(t *testing.T) {
+		s := newServer(t)
+		defer s.close()
+		s.exchangeResponse = ok("my-access-token1", "my-refresh-token2", expiresInSeconds)
+
+		authorizer := newAuthorizer(makeSlackClient(s.url()), clientID, clientSecret)
+
+		creds, err := authorizer.Exchange(context.Background(), s.authorizationCode, s.redirectURI)
+		require.NoError(t, err)
+		require.Equal(t, s.exchangeResponse.AccessToken, creds.AccessToken)
+		require.Equal(t, s.exchangeResponse.RefreshToken, creds.RefreshToken)
+		require.WithinDuration(t, time.Now().Add(time.Duration(expiresInSeconds)*time.Second), creds.ExpiresAt, 1*time.Second)
+	})
+
+	t.Run("ExchangeFail", func(t *testing.T) {
+		s := newServer(t)
+		defer s.close()
+		s.exchangeResponse = fail("invalid_code")
+
+		authorizer := newAuthorizer(makeSlackClient(s.url()), clientID, clientSecret)
+
+		_, err := authorizer.Exchange(context.Background(), s.authorizationCode, s.redirectURI)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "invalid_code")
+
+	})
+
+	t.Run("RefreshOK", func(t *testing.T) {
+		s := newServer(t)
+		defer s.close()
+		s.refreshResponse = ok("my-access-token2", "my-refresh-token3", expiresInSeconds)
+
+		authorizer := newAuthorizer(makeSlackClient(s.url()), clientID, clientSecret)
+
+		creds, err := authorizer.Refresh(context.Background(), refreshToken)
+		require.NoError(t, err)
+		require.Equal(t, s.refreshResponse.AccessToken, creds.AccessToken)
+		require.Equal(t, s.refreshResponse.RefreshToken, creds.RefreshToken)
+		require.WithinDuration(t, time.Now().Add(time.Duration(expiresInSeconds)*time.Second), creds.ExpiresAt, 1*time.Second)
+	})
+
+	t.Run("RefreshFail", func(t *testing.T) {
+
+		s := newServer(t)
+		defer s.close()
+		s.refreshResponse = fail("expired_token")
+
+		authorizer := newAuthorizer(makeSlackClient(s.url()), clientID, clientSecret)
+
+		_, err := authorizer.Refresh(context.Background(), refreshToken)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "expired_token")
+	})
+}
