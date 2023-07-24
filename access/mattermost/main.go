38d0aa4eec4b03226f42f2daddb0e3029ce55129
Lisa Kim <lisa@goteleport.com>
2023-07-24 11:55:41 -0700

    Refer to teleport repo's mattermost

--- access/mattermost/main.go
+++ access/mattermost/main.go
@@ -23,10 +23,14 @@ import (
 	"time"
 
 	"github.com/gravitational/kingpin"
+	"github.com/gravitational/teleport/integrations/access/mattermost"
 	"github.com/gravitational/teleport/integrations/lib"
 	"github.com/gravitational/teleport/integrations/lib/logger"
 	"github.com/gravitational/trace"
 )
+
+//go:embed example_config.toml
+var exampleConfig string
 
 func main() {
 	logger.Init()
@@ -64,7 +68,7 @@ func main() {
 }
 
 func run(configPath string, debug bool) error {
-	conf, err := LoadConfig(configPath)
+	conf, err := mattermost.LoadConfig(configPath)
 	if err != nil {
 		return trace.Wrap(err)
 	}
@@ -80,7 +84,7 @@ func run(configPath string, debug bool) error {
 		logger.Standard().Debugf("DEBUG logging enabled")
 	}
 
-	app, err := NewApp(*conf)
+	app, err := mattermost.NewMattermostApp(*conf)
 	if err != nil {
 		return trace.Wrap(err)
 	}
