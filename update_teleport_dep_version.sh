e3342bf764b3c620e4d2e14caaca6ebe0ea99937
Trent Clarke <trent@goteleport.com>
2023-08-02 09:25:26 +1000

    Adds local teleport repo to ref update script
    To help development when making changes and/or testing across the
    teleport and teleport-plugins modules, this PR adds the ability to
    point to a local clone of Teleport when running
    `update_teleport_dep_version.sh`.

--- update_teleport_dep_version.sh
+++ update_teleport_dep_version.sh
@@ -17,23 +17,31 @@ function sed_inline() {
 }
 
 version=$1
-[ -n "$version" ] || { echo "teleport version (tag like 'v1.2.3' or branch name like 'branch/v123') is required as the only argument to the script" && exit 1; }
+[ -n "$version" ] || { echo "teleport version (tag like 'v1.2.3', branch name like 'branch/v123', or the fully-qualified path to a local Teleport repository) is required as the only argument to the script" && exit 1; }
 
-ref="heads/$version"
-if [[ "$version" = v* ]]; then
-  ref="tags/$version"
+# If the $version *could* be a checked-out repository...
+if [[ -d "${version}/.git" ]]; then
+  # use that as the redirect path (for local development)
+  echo "Treating ${version} as a local checkout..."
+  sed_inline -e $"s#^\tgithub.com/gravitational/teleport => .*#\tgithub.com/gravitational/teleport =>  $version#" go.mod
+  sed_inline -e $"s#^\tgithub.com/gravitational/teleport/api => .*#\tgithub.com/gravitational/teleport/api => $version/api#" go.mod
+else
+  ref="heads/$version"
+  if [[ "$version" = v* ]]; then
+    ref="tags/$version"
+  fi
+
+  object_url=$(curl -sS --fail \
+    "https://api.github.com/repos/gravitational/teleport/git/ref/$ref" \
+    | jq -r .object.url)
+
+  object=$(curl -sS --fail "$object_url")
+  object_date=$(echo "$object" | jq -r .committer.date | sed 's/[-:TZ]//g')
+  object_sha="$(echo "$object" | jq -r .sha)"
+  pseudo_version="v0.0.0-${object_date}-${object_sha:0:12}"
+
+  sed_inline -e $"s#^\tgithub.com/gravitational/teleport => .*#\tgithub.com/gravitational/teleport => github.com/gravitational/teleport $pseudo_version // ref: $ref#" go.mod
+  sed_inline -e $"s#^\tgithub.com/gravitational/teleport/api => .*#\tgithub.com/gravitational/teleport/api => github.com/gravitational/teleport/api $pseudo_version // ref: $ref#" go.mod
 fi
-
-object_url=$(curl -sS --fail \
-  "https://api.github.com/repos/gravitational/teleport/git/ref/$ref" \
-  | jq -r .object.url)
-
-object=$(curl -sS --fail "$object_url")
-object_date=$(echo "$object" | jq -r .committer.date | sed 's/[-:TZ]//g')
-object_sha="$(echo "$object" | jq -r .sha)"
-pseudo_version="v0.0.0-${object_date}-${object_sha:0:12}"
-
-sed_inline -e $"s#^\tgithub.com/gravitational/teleport => .*#\tgithub.com/gravitational/teleport => github.com/gravitational/teleport $pseudo_version // ref: $ref#" go.mod
-sed_inline -e $"s#^\tgithub.com/gravitational/teleport/api => .*#\tgithub.com/gravitational/teleport/api => github.com/gravitational/teleport/api $pseudo_version // ref: $ref#" go.mod
 
 go mod tidy
